04ffab928ef27373cda1d8e039ae74f80d12dc6b
TAHA-CODEXX <tahastudying@gmail.com>
2025-12-29 11:46:55 +0100

    linking the project with mockapi to adding events and stock

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import axios from 'axios';
+const API_URL = 'https://69524f4e3b3c518fca123101.mockapi.io';
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
 
-export default function api() {
-  return (
-    <div>api</div>
-  )
-}
+// Events
+export const getEvents = () => api.get('/events');
+export const getEventById = (id) => api.get(`/events/${id}`);
+export const createEvent = (eventData) => api.post('/events', eventData);
+export const updateEvent = (id, eventData) => api.put(`/events/${id}`, eventData);
+export const deleteEvent = (id) => api.delete(`/events/${id}`);
+export default api;
+
